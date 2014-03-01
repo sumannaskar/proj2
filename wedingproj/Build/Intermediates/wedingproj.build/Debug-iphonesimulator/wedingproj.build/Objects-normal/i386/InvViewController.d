4164bcd44d1aecaa6ff7256b437f605386423470
suman naskar <suman.cse.imps@gmail.com>
2014-03-01 18:38:55 +0530

    ok

--- wedingproj/Build/Intermediates/wedingproj.build/Debug-iphonesimulator/wedingproj.build/Objects-normal/i386/InvViewController.d
+++ wedingproj/Build/Intermediates/wedingproj.build/Debug-iphonesimulator/wedingproj.build/Objects-normal/i386/InvViewController.d
@@ -1,6 +1,5 @@
 dependencies: \
   /Users/macmini2/Desktop/proj2/wedingproj/Invitation/InvViewController.m \
   /Users/macmini2/Desktop/proj2/wedingproj/Invitation/InvViewController.h \
-  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/SendIndividuallyViewController.h \
-  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/cell/Cell1.h \
+  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/SelectEvent/SelectEventViewController.h \
   /Users/macmini2/Desktop/proj2/wedingproj/Invitation/GroupWise/SelectGroup/SelectViewController.h
---
+++ wedingproj/Build/Intermediates/wedingproj.build/Debug-iphonesimulator/wedingproj.build/Objects-normal/i386/SelectEventViewController.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/SelectEvent/SelectEventViewController.m \
+  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/SelectEvent/SelectEventViewController.h \
+  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/SendIndividuallyViewController.h \
+  /Users/macmini2/Desktop/proj2/wedingproj/Invitation/Individually/cell/Cell1.h
