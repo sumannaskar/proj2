fa892a6a86433ef3623c0a9f77be3396acfde5b5
sumannaskar <micronixsystem@Micronixs-MacBook-Pro.local>
2014-03-04 19:34:23 +0530

    commit

--- wedingproj/Refer A Friend/ReferViewController.h
+++ wedingproj/Refer A Friend/ReferViewController.h
@@ -8,6 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ReferViewController : UIViewController
+@interface ReferViewController : UIViewController<UITextFieldDelegate>
+{
+    IBOutlet UITextView *commntstxtV;
+    NSDictionary *json;
+    NSString *check;
+}
+
+@property (retain, nonatomic) IBOutlet UITextField *nametxt;
+
+//@property (retain, nonatomic) IBOutlet UITextField *addresstxt;
+
+@property (retain, nonatomic) IBOutlet UITextField *emailtxt;
+
+//@property (retain, nonatomic) IBOutlet UITextField *contacttxt;
+
+@property (retain, nonatomic) IBOutlet UIImageView *backgoundImgview;
+
+@property (retain, nonatomic) IBOutlet UIButton *referencebtn;
+@property(nonatomic,retain)NSString *userId;
+
+- (IBAction)referencehit:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UIScrollView *scroll;
+
+@property(nonatomic,retain)NSString *refStatus;
 
 @end
