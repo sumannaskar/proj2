3ccb3562992f359a4d29c2d5e55e3e7c9fce6ba2
sumannaskar <suman.cse.imps@gmail.com>
2014-03-05 16:30:48 +0530

    cbcv

--- wedingproj/RSVP/Details/DestailsRSVPViewController.h
+++ wedingproj/RSVP/Details/DestailsRSVPViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface DestailsRSVPViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITableView *rsvpTable;
+@property (strong, nonatomic) IBOutlet UIImageView *backImgv;
 
 @end
--- wedingproj/RSVP/Details/DestailsRSVPViewController.m
+++ wedingproj/RSVP/Details/DestailsRSVPViewController.m
@@ -34,5 +34,44 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return 5;
+}
+//for normal table view....
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+
+     static NSString *cellidentifire;
+     UITableViewCell *cell;
+     cell=[tableView dequeueReusableCellWithIdentifier:cellidentifire];
+     if(cell==nil)
+     {
+         cell= [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellidentifire];
+         cell.textLabel.text=@"guest";
+         
+         UILabel *status = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 72, 37)];
+         status.text = @"Yes";
+         
+         cell.accessoryType = UITableViewCellAccessoryNone;
+         cell.accessoryView = status;
+         //[cell setSelectionStyle:UITableViewCellSelectionStyleGray];
+         return cell;
+     }
+     return cell;
+
+//[NSString stringWithFormat:@"%d",a];
+
+ }
+
 
 @end
