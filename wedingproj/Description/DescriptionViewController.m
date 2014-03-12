28eb00e69c691f476c63617264ce7c83199ce2e0
sumannaskar <micronixsystem@Micronixs-MacBook-Pro.local>
2014-03-12 16:44:36 +0530

    commit

--- wedingproj/Description/DescriptionViewController.m
+++ wedingproj/Description/DescriptionViewController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self image];
     descriptxtview.text=self.descriptiontxt;
 }
 
@@ -35,5 +36,46 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark -
+#pragma mark - dispatching different images for different devices
+
+-(void)image
+{
+    if([[UIScreen mainScreen] bounds].size.height  < 600)
+    {
+        
+        
+        
+        if ([[UIScreen mainScreen] bounds].size.height == 568)
+        {
+            [backgroundimgV setImage:[UIImage imageNamed:@"normal-background-320.jpg"]];
+            
+        }
+        else if ([[UIScreen mainScreen] bounds].size.height == 480)
+        {
+            [backgroundimgV setImage:[UIImage imageNamed:@"normal-background-320.jpg"]];
+            
+        }
+        else
+        {
+            [backgroundimgV setImage:[UIImage imageNamed:@"normal-background-320.jpg"]];
+            
+        }
+        
+        
+    }
+    else
+    {
+        if ([[UIScreen mainScreen] bounds].size.height == 1024)
+        {
+            [backgroundimgV setImage:[UIImage imageNamed:@"768-1004.jpg"]];
+            
+            
+        }
+        
+    }
+}
+
+
 
 @end
