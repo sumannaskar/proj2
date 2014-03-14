d6d9bd2d6bda7ea774f4f649e860554d2354b844
sumannaskar <micronixsystem@Micronixs-MacBook-Pro.local>
2014-03-14 15:55:26 +0530

    dfgvdfgvdvd

--- wedingproj/Features/FeatureViewController.h
+++ wedingproj/Features/FeatureViewController.h
@@ -9,6 +9,27 @@
 #import <UIKit/UIKit.h>
 
 @interface FeatureViewController : UIViewController
+{
+    
+    IBOutlet UIButton *guestbtn;
+    
+    IBOutlet UIButton *groupbtn;
+    
+    IBOutlet UIButton *invitationbtn;
+    
+    IBOutlet UIButton *eventbtn;
+    
+    
+    IBOutlet UIButton *todolistbtn;
+    
+    IBOutlet UIButton *budgetbtn;
+    
+    IBOutlet UIButton *vendorbtn;
+    
+    IBOutlet UIButton *rsvpbtn;
+    
+    IBOutlet UIImageView *backgroundimgV;
+}
 - (IBAction)eventactn:(UIButton *)sender;
 
 - (IBAction)todolistactn:(UIButton *)sender;
@@ -19,4 +40,6 @@
 - (IBAction)guestlistactn:(UIButton *)sender;
 - (IBAction)invitationactn:(UIButton *)sender;
 
+- (IBAction)rsvpactn:(UIButton *)sender;
+- (IBAction)groupactn:(UIButton *)sender;
 @end
