5b3b2488d8fd954db74aa9b4187ce69d3254e4e1
sumannaskar <suman.cse.imps@gmail.com>
2014-03-14 16:11:36 +0530

    oik

--- wedingproj/Budget/CalculateBudget/CalculateBudget.m
+++ wedingproj/Budget/CalculateBudget/CalculateBudget.m
@@ -30,122 +30,122 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    json=[[NSMutableArray alloc]init];
-    
-    yourbudgettext.text =  [NSString stringWithFormat:@"%ld", (long)[[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"]] ;
-    NSLog(@"%ld",(long)[[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"]);
+//    json=[[NSMutableArray alloc]init];
+//    
+//    yourbudgettext.text =  [NSString stringWithFormat:@"%ld", (long)[[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"]] ;
+//    NSLog(@"%ld",(long)[[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"]);
     
     
    
-    [self recievedata];
+   // [self recievedata];
     
     
 }
 
-
- -(void)fetchedData:(NSData *)responseData
-{
-    NSError *error;
-    json = [NSJSONSerialization
-            JSONObjectWithData:responseData //1
-            
-            options:kNilOptions
-            error:&error];
-    
-    allocatetext.text=[json valueForKey:@"allocated_budget"];
-    balancemaintext.text=[json valueForKey:@"remaining_budget"];
-    
-}
-
-
-
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    
-    [textField resignFirstResponder];
-    
-    return YES;
-}
-
--(void)recievedata
-{
-    
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@user_id=%@&total=%@&apikey=micronix_10_2014_wedsimple_proj",URL,[SSKeychain passwordForService:@"LoginViewController" account:@"User"],yourbudgettext.text]];
-    
-    // [HUD showUIBlockingIndicatorWithText:@"Loading.."];
-    dispatch_async
-    (kBgQueue, ^
-     {
-         NSData* data = [NSData dataWithContentsOfURL:url];
-         NSString *tempstring = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-         
-         
-         if (data.length<1 || [tempstring isEqualToString:@"null"])
-         {
-             
-             
-             //[self performSelectorOnMainThread:@selector(serverFail) withObject:nil waitUntilDone:YES];
-             
-         }
-         
-         else
-         {
-             [self performSelectorOnMainThread:@selector(fetchedData:)
-                                    withObject:data waitUntilDone:YES];
-             
-         }
-     }
-     );
-
-    
-}
--(void)senddata
-{
-    
-}
-
-
--(void)retrivedata
-{
-    if ([yourbudgettext.text intValue] ==[[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"]) {
-        
-    }
-    else
-    {
-        [self savedata];
-    }
-}
-
--(void)savedata
-{
-    if (yourbudgettext.text.length > 0) {
-        [[NSUserDefaults standardUserDefaults] setInteger:[yourbudgettext.text intValue] forKey:@"HighScoreSaved"];
-    }
-    else
-    {
-        [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"HighScoreSaved"];
-    }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)done:(id)sender {
-    
-    
-    [self recievedata];
-    [self retrivedata];
-    
-}
-
-- (IBAction)calculate:(id)sender {
-    
-    
-    
-    
-    
-}
+//
+// -(void)fetchedData:(NSData *)responseData
+//{
+//    NSError *error;
+//    json = [NSJSONSerialization
+//            JSONObjectWithData:responseData //1
+//            
+//            options:kNilOptions
+//            error:&error];
+//    
+//    allocatetext.text=[json valueForKey:@"allocated_budget"];
+//    balancemaintext.text=[json valueForKey:@"remaining_budget"];
+//    
+//}
+//
+//
+//
+//
+//- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+//    
+//    [textField resignFirstResponder];
+//    
+//    return YES;
+//}
+//
+//-(void)recievedata
+//{
+//    
+//    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@user_id=%@&total=%@&apikey=micronix_10_2014_wedsimple_proj",URL,[SSKeychain passwordForService:@"LoginViewController" account:@"User"],yourbudgettext.text]];
+//    
+//    // [HUD showUIBlockingIndicatorWithText:@"Loading.."];
+//    dispatch_async
+//    (kBgQueue, ^
+//     {
+//         NSData* data = [NSData dataWithContentsOfURL:url];
+//         NSString *tempstring = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//         
+//         
+//         if (data.length<1 || [tempstring isEqualToString:@"null"])
+//         {
+//             
+//             
+//             //[self performSelectorOnMainThread:@selector(serverFail) withObject:nil waitUntilDone:YES];
+//             
+//         }
+//         
+//         else
+//         {
+//             [self performSelectorOnMainThread:@selector(fetchedData:)
+//                                    withObject:data waitUntilDone:YES];
+//             
+//         }
+//     }
+//     );
+//
+//    
+//}
+//-(void)senddata
+//{
+//    
+//}
+//
+//
+//-(void)retrivedata
+//{
+//    if ([yourbudgettext.text intValue] ==[[NSUserDefaults standardUserDefaults] integerForKey:@"HighScoreSaved"]) {
+//        
+//    }
+//    else
+//    {
+//        [self savedata];
+//    }
+//}
+//
+//-(void)savedata
+//{
+//    if (yourbudgettext.text.length > 0) {
+//        [[NSUserDefaults standardUserDefaults] setInteger:[yourbudgettext.text intValue] forKey:@"HighScoreSaved"];
+//    }
+//    else
+//    {
+//        [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"HighScoreSaved"];
+//    }
+//}
+//
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+//
+//- (IBAction)done:(id)sender {
+//    
+//    
+//    [self recievedata];
+//    [self retrivedata];
+//    
+//}
+//
+//- (IBAction)calculate:(id)sender {
+//    
+//    
+//    
+//    
+//    
+//}
 @end
